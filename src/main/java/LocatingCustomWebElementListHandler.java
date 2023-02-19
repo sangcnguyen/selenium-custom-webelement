09ed3510d38cafb0b7a4b8969861b7443cd6cd90
Sang Nguyen <ngcongsang@gmail.com>
2023-02-19 23:41:35 +0700

    done test

--- src/main/java/LocatingCustomWebElementListHandler.java
+++ src/main/java/LocatingCustomWebElementListHandler.java
@@ -2,7 +2,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.pagefactory.ElementLocator;
@@ -20,14 +19,12 @@ public class LocatingCustomWebElementListHandler implements InvocationHandler {
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
     List<WebElement> elements = locator.findElements();
-    System.out.println("test");
     List<CustomWebElementImpl> customWebElements = elements.stream()
         .map(element -> new CustomWebElementImpl(element, webDriver))
-        .collect(Collectors.toList());
+        .toList();
 
     try {
-      System.out.println("test 2");
-      return method.invoke(customWebElements, proxy);
+      return method.invoke(customWebElements, args);
     } catch (InvocationTargetException e) {
       throw e.getCause();
     }
